327a78031e17e52f7176c2715fc0bfcffe499417
maskikuy <kproject305@gmail.com>
2024-06-03 14:25:20 +0700

    menulis while

--- IOFile/IOFile.cpp
+++ IOFile/IOFile.cpp
@@ -10,6 +10,14 @@ int main() {
 	ofstream outfile;
 	// menunjuk ke sebuah nama file
 	outfile.open("contohfile.txt");
-
+	cout << ">= menulis, \'q\' untuk keluar" << endl;
+	//unlimited loop untuk menulis
+	while (true) {
+		cout << "- ";
+		// mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akan berhenti jika anda memasukan karakter q
+		//menuliskan dan memasukan nilai dari'baris' ke dalam file
+	}
 
 }
