f30bf308180313c09552286afb643771c659d666
maskikuy <kproject305@gmail.com>
2024-06-03 14:48:01 +0700

    mendapatkan setiap karakter dalam satu baris

--- IOFileWithPath/IOFileWithPath.cpp
+++ IOFileWithPath/IOFileWithPath.cpp
@@ -9,3 +9,16 @@ int main() {
 
 	cout << "Masukkan Nama file : ";
 	cin >> NamaFile;
+
+	// membuka file dalam mode menulis.
+	ofstream outfile;
+	// menunjuk ke sebuah nama file
+	outfile.open(NamaFile + ".txt", ios::out);
+
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+	//unlimited loop untuk menulis
+	while (true) {
+		cout << "- ";
+		// mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
