ddd9959fc41b449f266e7c52df04a8703082c336
maskikuy <kproject305@gmail.com>
2024-06-03 15:07:49 +0700

    blok di eksekusi

--- ManualException/ManualException.cpp
+++ ManualException/ManualException.cpp
@@ -8,4 +8,13 @@ int main()
 		//throw 0.5; //melemparkan sebuah integer maka
 		cout << "pernyataan tidak akan dieksekusi" << endl;
 	}
+	catch (int a) {
+		//blok ini akan di eksekusi
+		cout << "Pengecualian akan dieksekusi" << endl;
+	}
+	catch (...) {
+		cout << "Default Pengecualian dieksekusi" << endl;
+
+	}
+	return 0;
 }
